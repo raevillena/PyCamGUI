1a7b6104a7042d792b2b69bd2675758c0f428af8
raevillena <rhaey1995@gmail.com>
2024-07-08 16:50:56 +0800

    adding more scripts as version for tflite

--- eval_tflite2.py
+++ eval_tflite2.py
@@ -1,26 +1,30 @@
 from tflite_runtime.interpreter import Interpreter 
-from PIL import Image
 import numpy as np
+import tensorflow as tf
+from PIL import Image
 import time
+
 
 def load_labels(path): # Read the labels from the text file as a Python list.
   with open(path, 'r') as f:
     return [line.strip() for i, line in enumerate(f.readlines())]
-
+  
 def set_input_tensor(interpreter, image):
   tensor_index = interpreter.get_input_details()[0]['index']
   input_tensor = interpreter.tensor(tensor_index)()[0]
   input_tensor[:, :] = image
 
+
 def classify_image(interpreter, image, top_k=1):
+  #interpreter.set_tensor(input_tensor, image)
   set_input_tensor(interpreter, image)
 
   interpreter.invoke()
-  output_details = interpreter.get_output_details()[0]
   output = np.squeeze(interpreter.get_tensor(output_details[0]['index']))
+  #output_data = interpreter.get_tensor(output_details[0]['index'])
 
-  scale, zero_point = output_details['quantization']
-  output = scale * (output - zero_point)
+  #scale, zero_point = output_details['quantization']
+  #output = scale * (output - zero_point)
 
   ordered = np.argpartition(-output, 1)
   return [(i, output[i]) for i in ordered[:top_k]][0]
@@ -31,13 +35,13 @@ model_path = '/home/admin/Model_1_EfficientNetB0_with_fine-tuning.tflite'
 label_path = data_folder + "classes.txt"
 
 interpreter = Interpreter(model_path)
-print("Model Loaded Successfully.")
-
 interpreter.allocate_tensors()
-_, height, width, _ = interpreter.get_input_details()[0]['shape']
-print("Image Shape (", width, ",", height, ")")
 
-# Load an image to be classified.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+_,height,width,_ = input_details[0]['shape']
+
 image = Image.open(data_folder + "3.jpg").convert('RGB').resize((width, height))
 
 # Classify the image.
