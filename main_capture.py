2bfb9abfaba1f11de5bdf03c8f7d09bb11b6514c
raevillena <rhaey1995@gmail.com>
2024-07-11 10:58:17 +0800

    removing pyqt as not performing well

--- main_capture.py
+++ main_capture.py
@@ -5,11 +5,6 @@ import os
 import time
 import numpy as np
 
-from PyQt5 import QtCore
-from PyQt5.QtWidgets import (QApplication, QHBoxLayout, QLabel, QPushButton,
-                             QVBoxLayout, QWidget)
-from picamera2.previews.qt import QGlPicamera2
-
 # folder path
 dir_path = r'/home/admin/PyCamGUI/tmp/'
 
@@ -27,45 +22,10 @@ config_capture = picam2.create_still_configuration(main={'size': full},
 
 config_preview = picam2.create_preview_configuration({"size": maxx})
 
-#app main
-
-app = QApplication([])
-def on_button_clicked():
-    button.setEnabled(False)
-    cam_stop()
-
-
-def capture_done(job):
-    picam2.wait(job)
-    button.setEnabled(True)
-
-#camera port
-qpicamera2 = QGlPicamera2(picam2, width=800, height=400, keep_ar=True)
-qpicamera2.setWindowTitle("Qt Picamera2 App")
-
-#app components
-button = QPushButton("Exit Alignment")
-label = QLabel()
-window = QWidget()
-qpicamera2.done_signal.connect(capture_done)
-button.clicked.connect(on_button_clicked)
-
-label.setFixedWidth(400)
-label.setAlignment(QtCore.Qt.AlignTop)
-layout_h = QHBoxLayout()
-layout_v = QVBoxLayout()
-layout_v.addWidget(label)
-layout_v.addWidget(button)
-layout_h.addWidget(qpicamera2, 80)
-layout_h.addLayout(layout_v, 20)
-window.setWindowTitle("Qt Picamera2 App")
-window.resize(800, 400)
-window.setLayout(layout_h)
-
 
 def cam_preview_show():
     picam2.configure(config_preview)
-    #picam2.start_preview(Preview.QTGL, x=0, y=0, width=800, height=400)
+    picam2.start_preview(Preview.QTGL, x=0, y=0, width=800, height=400)
     picam2.start()
     picam2.set_controls({"AfMode":controls.AfModeEnum.Continuous,
                     "LensPosition":1.0,
@@ -77,10 +37,7 @@ def cam_preview_show():
     overlay[:150, 200:] = (255, 0, 0, 64) # reddish
     overlay[150:, :200] = (0, 255, 0, 64) # greenish
     overlay[150:, 200:] = (0, 0, 255, 64) # blueish
-    #picam2.set_overlay(overlay)
-    qpicamera2.set_overlay(overlay)
-    window.show()
-    app.exec()
+    picam2.set_overlay(overlay)
 
 def cam_preview():
     picam2.configure(config_preview)
